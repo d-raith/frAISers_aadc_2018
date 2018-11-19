daecf3d95e9d7bb46951dd1b8151de9fa489234e
Crenshaw17 <34373652+Crenshaw17@users.noreply.github.com>
2018-11-19 17:09:05 +0100

    Update NNThriftFilter.h

--- src/aadcUser/NN/NNThriftFilter/NNThriftFilter.h
+++ src/aadcUser/NN/NNThriftFilter/NNThriftFilter.h
@@ -2,10 +2,6 @@
 /******************************************************************************
  * Audi Autonomous Driving Cup 2018
  * Team frAIsers
- * AUTHOR: Fabien Jenne
- *
- * Thrift filter implementation
- *
 ******************************************************************************/
 
 #pragma once
@@ -20,38 +16,21 @@
 #include <cstdlib>
 #include <sstream>
 #include <utility>
-#include <mutex>                                // NOLINT
-
-// #include <torch/script.h> // One-stop header.
-// #include <torch/torch.h>
-
+#include <mutex>                           
 #include "stdafx.h"
 #include "../NNThrift/gen-cpp/NNComm.h"
 
-using namespace ::apache::thrift;               // NOLINT
-using namespace ::apache::thrift::protocol;     // NOLINT
-using namespace ::apache::thrift::transport;    // NOLINT
-using namespace nn_thrift;                      // NOLINT
-using namespace cv;                             // NOLINT
+using namespace ::apache::thrift;             
+using namespace ::apache::thrift::protocol;   
+using namespace ::apache::thrift::transport; 
+using namespace nn_thrift;                    
+using namespace cv;                      
 
 #define CID_NN_THRIFT_FILTER "NN.thrift.adtf.cid"
 #define LABEL_NN_THRIFT_FILTER "Neural Network Thrift Filter"
 
 
 class NNThriftFilter : public adtf::filter::ant::cConfigurableFilter {
-
-    // class seg_network {
-    //  public:
-    //     seg_network();
-    //     std::pair<cv::Mat, cv::Mat> infer(torch::Tensor tensor_image);
-    //     torch::Tensor preprocess(cv::Mat rgb);
-    //  private:
-    //     bool _use_cuda;
-    //     torch::Device _device;
-    //     std::shared_ptr<torch::jit::script::Module> _module;
-    //     cv::Scalar _dataset_mean;
-    // };
-
  public:
     NNThriftFilter();
 
@@ -62,47 +41,30 @@ class NNThriftFilter : public adtf::filter::ant::cConfigurableFilter {
                             REQUIRE_INTERFACE(adtf::services::IReferenceClock));
 
  private:
-    //  ADTF
     property_variable<tBool> m_propEnableConsoleOutput = tTrue;
     property_variable<bool> launch_nn_server = false;
     property_variable<bool> run_network_locally = false;
     object_ptr<adtf::services::IReferenceClock> m_pClock;
     runnable<> m_oRunnable;
     tTimeStamp tmTimeOfLastActivation;
-
-    // Pins
     cPinReader m_oReaderImage;
     cPinWriter m_oWriterImage;
-
-    // Media description
     adtf::streaming::tStreamImageFormat m_sImageFormat;
-
-    // Thrift
-    stdcxx::shared_ptr<TTransport> _thrift_transport_client;  // does not have to be deleted
+    stdcxx::shared_ptr<TTransport> _thrift_transport_client;
     NNCommClient* _thrift_nn_client = NULL;
     bool is_connected_to_nn_server = false;
-    std::string thrift_nn_server = "127.0.0.1";  // "192.168.168.104";  // 192.168.167.148, 10.8.105.255
+    std::string thrift_nn_server = "127.0.0.1";
     int thrift_nn_server_port = 9093;
-
-    // Media descriptions
-
 public:
-    // implement cFilterBase (implements cFilterLevelmachine)
     tResult Init(tInitStage eStage);
     tResult Start();
     tResult Stop();
     tResult Shutdown(tInitStage eStage);
     tResult RunTrigger(tTimeStamp tmTimeofActivation);
-
-    // Thrift client
     bool connectToNNServer();
 
 private:
-    // Thrift
-    //   Client
     nn_thrift::NNImage getNNOutput(const nn_thrift::NNImage& img);
-
-    // filter
     tResult runNNPipeline();
     tResult readInputPin(cv::Mat* image);
     tResult writeOutputPin(const Mat& outputImage);
